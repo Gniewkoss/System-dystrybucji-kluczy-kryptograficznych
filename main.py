0f32c4577484f98c245e6645ea5f42d7c75a27ca
Gorbkos <gniewkoscielak@gmail.com>
2024-04-24 15:42:26 +0200

    func

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-def check_prime(p):
-    for i in range(2,p):
-        if p%i == 0:
-            return False
-    return True
-
-#
-# def create_key (P,G,a,b):
-#     x= G**a % P
-#     y= G**b % P
-#
-#     ka = y**a % P
-#     kb = x**b % P
-#     return ka, kb
+import random
 
 
-# print("Shared secret:" + str(create_key(2,1,3,5)))
+def diffie_hellman():
+    p = 23  # liczba pierwsza
+    g = 5  # liczba pierwotna
 
+    #klucz prywatny
+    a = random.randint(1, p - 1)
+    b = random.randint(1, p - 1)
+
+    #wartość publiczną
+    A = pow(g, a, p)
+    B = pow(g, b, p)
+
+    shared_key_alice = pow(B, a, p)
+    shared_key_bob = pow(A, b, p)
+
+    print(f"Wartość publiczna Alice (A): {A}")
+    print(f"Wartość publiczna Bob (B): {B}")
+    print(f"Klucz wspólny obliczony przez Alice: {shared_key_alice}")
+    print(f"Klucz wspólny obliczony przez Bob: {shared_key_bob}")
+
+    if shared_key_alice == shared_key_bob:
+        print("Alice i Bob mają ten sam klucz wspólny!")
+    else:
+        print("Coś poszło nie tak, klucze nie są identyczne.")
+
+
+diffie_hellman()
